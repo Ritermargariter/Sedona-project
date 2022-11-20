481e949deff96fa894cd3f6e3b419273147f6ba1
tatarchuk_alexey <tatarchuk_alexey@forecsys.ru>
2022-11-20 21:06:42 +0300

    finished form html

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,21 @@
+let navMain = document.querySelector('.header-navigation');
+let navToggle = document.querySelector('.header-navigation__toggle');
+
+navMain.classList.remove('header-navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header-navigation--closed')) {
+    navMain.classList.remove('header-navigation--closed');
+    navMain.classList.add('header-navigation--opened');
+  } else {
+    navMain.classList.add('header-navigation--closed');
+    navMain.classList.remove('header-navigation--opened');
+  }
+});
+
+
+
+
 let buttonShow = document.querySelector('.search-hotel__button');
 let formSearch = document.querySelector('.form-search');
 
@@ -62,18 +80,5 @@ window.addEventListener('keydown', function (evt) {
 })
 
 
-let navMain = document.querySelector('.header-navigation');
-let navToggle = document.querySelector('.header-navigation__toggle');
 
-navMain.classList.remove('header-navigation--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('header-navigation--closed')) {
-    navMain.classList.remove('header-navigation--closed');
-    navMain.classList.add('header-navigation--opened');
-  } else {
-    navMain.classList.add('header-navigation--closed');
-    navMain.classList.remove('header-navigation--opened');
-  }
-});
 
