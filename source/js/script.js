ff56e1fa880bc9e445716768e26a268dbc5c8514
Margo <borodulina.margo@mail.ru>
2022-11-28 18:34:51 +0300

    fixed form.html

--- source/js/script.js
+++ source/js/script.js
@@ -16,69 +16,6 @@ navToggle.addEventListener('click', function () {
 
 
 
-let buttonShow = document.querySelector('.search-hotel__button');
-let formSearch = document.querySelector('.form-search');
-
-let dateArrival = formSearch.querySelector('#date-arrival');
-let dateLeave = formSearch.querySelector('#date-leave');
-let ticketAdult = formSearch.querySelector('#adult-ticket');
-let ticketChildren = formSearch.querySelector('#children-ticket');
-
-var isStorageSupport = true;
-var storage = '';
-
-/* Доступен ли localStorage */
-try {
-  storage = localStorage.getItem('ticketAdult');
-} catch (err) {
-  isStorageSupport = false;
-}
-
-/* Скрываем открытую форму по умолчанию */
-formSearch.classList.remove('form-search--show');
-formSearch.classList.add('form-search--hide');
-
-/* Открытие и закрытие формы по кнопке */
-buttonShow.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  formSearch.classList.toggle('form-search--hide');
-  formSearch.classList.toggle('form-search--show');
-  formSearch.classList.remove('form-search--error');
-  dateArrival.focus();
-
-  ticketAdult.value = localStorage.getItem('ticketAdult');
-  ticketChildren.value = localStorage.getItem('ticketChildren');
-});
-
-/* Проверка заполненности полей формы перед отправкой,
-тряска формы при отсутствии данных,
-загрузках данных из localStorage, если доступно */
-formSearch.addEventListener('submit', function (evt) {
-  if (!dateArrival.value || !dateLeave.value || !ticketAdult.value || !ticketChildren.value) {
-    evt.preventDefault();
-    formSearch.classList.remove('form-search--error');
-    formSearch.offsetWidth = formSearch.offsetWidth;
-    formSearch.classList.add('form-search--error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('ticketAdult', ticketAdult.value);
-      localStorage.setItem('ticketChildren', ticketChildren.value);
-    }
-  }
-});
-
-/* Закрытие формы клавишой ESC */
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (formSearch.classList.contains('form-search--show')) {
-      evt.preventDefault();
-      formSearch.classList.toggle('form-search--hide');
-      formSearch.classList.toggle('form-search--show');
-      formSearch.classList.remove('form-search--error');
-    }
-  }
-})
-
 
 
 
